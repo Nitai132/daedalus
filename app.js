c1863246badcb1c369e63ad2b52c33d289185285
Your Name <you@example.com>
2020-10-08 02:26:32 +0300

    3rd sql

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 mysql.createConnection({
-      host: 'us-cdbr-east-02.cleardb.com',
-      port: 3006,
-      user: 'b6b84acbfe103d',
-      password: '2ca1b309',
-      database: 'heroku_4a5144a55c9bce4'
+      HOST: 'us-cdbr-east-02.cleardb.com',
+      USER: 'b6b84acbfe103d',
+      PASSWORD: '2ca1b309',
+      DB: 'heroku_4a5144a55c9bce4'
     }).then(connection => {
         global.mysqlConnection = connection;
         app.listen({port}, () => {
