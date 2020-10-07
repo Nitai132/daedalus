1acc18c4259757e3895a1765e4e841a0a14cb9c0
Your Name <you@example.com>
2020-10-08 02:37:13 +0300

    4th mysql

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 // const cors = require('cors');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql');
 const mainController = require('./controller/main-controller')
 const path = require('path');
 const port = process.env.PORT || 80
@@ -12,19 +12,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-mysql.createConnection({
+
+const connection = mysql.createConnection({
       HOST: 'us-cdbr-east-02.cleardb.com',
       USER: 'b6b84acbfe103d',
       PASSWORD: '2ca1b309',
       DB: 'heroku_4a5144a55c9bce4'
-    }).then(connection => {
-        global.mysqlConnection = connection;
-        app.listen({port}, () => {
-              console.log(`app is on port: ${port}`);
-        });
 });
+    
+connection.connect();
+
+// mysql.createConnection({
+//       HOST: 'us-cdbr-east-02.cleardb.com',
+//       USER: 'b6b84acbfe103d',
+//       PASSWORD: '2ca1b309',
+//       DB: 'heroku_4a5144a55c9bce4'
+//     }).then(connection => {
+//         global.mysqlConnection = connection;
+
+// });
+
+
 
 app.use('/', mainController);
+
+app.listen({port}, () => {
+      console.log(`app is on port: ${port}`);
+});
 
 process.on('uncaughtException', (err, origin) => {
     console.log(err);
