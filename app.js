24919f833b2df5d604bafb32ec0a7e63c5cdcac3
Your Name <you@example.com>
2020-10-08 11:30:11 +0300

    plz this 1

--- app.js
+++ app.js
@@ -22,17 +22,38 @@ app.use(bodyParser.json());
 // });
     
 // connection.connect();
-
-mysql.createConnection({
+let db_config = {
       host: 'us-cdbr-east-02.cleardb.com',
-      port: 3006,
       user: 'b6b84acbfe103d',
       password: '2ca1b309',
       database: 'heroku_4a5144a55c9bce4'
-    }).then(connection => {
-        mysqlConnection = connection;
-});
+}
 
+
+let connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    global.connection = mysql.createConnection(db_config); 
+											
+
+    connection.connect(function(err) {             
+        if (err) {                                 
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); 
+        }                
+    });                           
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();     
+        } else {                                    
+            throw err;                   
+        }
+    });
+}
+
+handleDisconnect();
 
 
 app.use('/', mainController);
