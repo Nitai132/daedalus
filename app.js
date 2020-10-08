f16ff3e9ae908b7b146f7565c0d5aa56e77aa26f
Your Name <you@example.com>
2020-10-08 10:51:49 +0300

    sql

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 // const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const mainController = require('./controller/main-controller')
 const path = require('path');
 const port = process.env.PORT || 80
@@ -13,25 +13,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-const connection = mysql.createConnection({
-      HOST: 'us-cdbr-east-02.cleardb.com',
-      PORT: 3006,
-      USER: 'b6b84acbfe103d',
-      PASSWORD: '2ca1b309',
-      DB: 'heroku_4a5144a55c9bce4'
-});
-    
-connection.global.connect();
-
-// mysql.createConnection({
+// const connection = mysql.createConnection({
 //       HOST: 'us-cdbr-east-02.cleardb.com',
+//       PORT: 3006,
 //       USER: 'b6b84acbfe103d',
 //       PASSWORD: '2ca1b309',
 //       DB: 'heroku_4a5144a55c9bce4'
-//     }).then(connection => {
-//         global.mysqlConnection = connection;
-
 // });
+    
+// connection.connect();
+
+mysql.createConnection({
+      HOST: 'us-cdbr-east-02.cleardb.com',
+      USER: 'b6b84acbfe103d',
+      PASSWORD: '2ca1b309',
+      DB: 'heroku_4a5144a55c9bce4'
+    }).then(connection => {
+        global.mysqlConnection = connection;
+});
 
 
 
